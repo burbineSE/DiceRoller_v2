12d17e3fea0b331ed812e3376f8170b91212f483
Chris Burbine <chris.t.burbine@gmail.com>
2019-04-25 14:17:13 -0400

    First commit of V2

---
+++ DiceRoller_v2/Pages/DiceRoller.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DiceRoller_v2.Pages
+{
+    public class DiceRoller : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
